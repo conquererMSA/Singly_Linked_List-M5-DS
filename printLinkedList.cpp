8e4083a351946c6946afbf0a20f87b58e8a034f3
asus test <msa-99@outlook.com>
2024-06-23 14:21:38 +0600

    print linledList

--- printLinkedList.cpp
+++ printLinkedList.cpp
@@ -9,7 +9,7 @@ class Node {
         this->val=val;
     }
 };
-//ekhane Node hocc ekti user defigned variable zar moddhye val and next nam e duita variable thake. val r moddhye obj er nij value and next er noddhye next node er pointer hold kore rakhe.
+//ekhane Node hocce ekti user defigned variable zar moddhye val and next nam e duita variable thake. val r moddhye obj er nij value and next er noddhye next node er pointer hold kore rakhe.
 int main(){
    Node* head=new Node(12);
    Node* a=new Node(13);
@@ -19,8 +19,9 @@ int main(){
    Node* temp=head;
     //ekhane LinkedList er first node head er memory/pointer temp er moddhye rakha hoyeche. temp er moddhye val and next ache. ai next e abar a er next and val ache. a er next e abar b er val and next aceh.
     while(temp!=NULL){
-        //ekhane loop hote hote zokhon last node d te zabe tokhon d er next value hocce NULL;
-        
+        //ekhane loop hote hote zokhon last node d te zabe tokhon d er next value hocce NULL. kintu last node er ager sob gulu node e next er value hocce val and next.
+        cout<<temp->val<<endl;
+        temp=temp->next;
     }
 
     return 0;
