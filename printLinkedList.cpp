666a488b7091d8736811d4e43eb7dc88a540828a
asus test <msa-99@outlook.com>
2024-06-23 14:05:06 +0600

    print linledList

---
+++ printLinkedList.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+/* */
+class Node {
+    public:
+    int val;
+    Node* next=NULL;
+    Node(int val){
+        this->val=val;
+    }
+};
+//ekhane Node hocc ekti user defigned variable zar moddhye val and next nam e duita variable thake. val r moddhye obj er nij value and next er noddhye next node er pointer hold kore rakhe.
+int main(){
+
+    return 0;
+}
