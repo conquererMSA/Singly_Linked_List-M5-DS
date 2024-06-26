801cfa91969ddba6b0b1934d2b9101ec326ff8a0
asus test <msa-99@outlook.com>
2024-06-26 13:06:50 +0600

    insert at position reacap

--- linkedListReacap.cpp
+++ linkedListReacap.cpp
@@ -9,12 +9,42 @@ class Node{
         this->next=NULL;
     }
 };
+int countSize(Node* head){
+ int count=0;
+  while(head!=NULL){
+    count++;
+    head=head->next;
+  }
+  return count;
+}
+
+void insertAtPosition(Node *&head,int pos, int val){
+    Node* newNode=new Node(val);
+    Node* temp=head;
+    if(pos==1){
+        newNode->next=temp;
+        head=newNode;
+        return;
+    }
+    else if(pos>countSize(head)){
+        cout<<"Invalid position"<<endl;
+        return;
+    }else{
+       for(int i=1; i<pos-1; i++){
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+   }
+    
+};
 void printLinkedList(Node* head){
     Node* temp=head;
     while(temp!=NULL){
         cout<<temp->val<<" ";
         temp=temp->next;
     }
+    cout<<endl;
 }
 int main(){
     Node* head=new Node(1);
@@ -26,5 +56,10 @@ int main(){
     a->next=b;
     b->next=c;
     c->next=d;
+    int pos, val;
+    cin>>pos>>val;
+    printLinkedList(head);
+    insertAtPosition(head,pos,val);
+    printLinkedList(head);
     return 0;
 }
